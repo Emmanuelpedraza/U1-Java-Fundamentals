d6da436f614c1a426534e7d0dd81cd1561c5cdd6
1103502004EPp <pedraemm0012warren.k12.in.us>
2021-08-19 12:52:15 -0400

    l.6 l.7 complete

--- src/L7.java
+++ src/L7.java
@@ -1,8 +1,8 @@
 import java.text.DecimalFormat;  //importing the class from the package
+//import java.text.*;
+public class L7 {
 
-public class L7{
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         //creates a decimal object roundTo2 and initializes it.
         DecimalFormat roundTo2 = new DecimalFormat("0.00");
@@ -23,5 +23,10 @@ public class L7{
 
         //using the roundTo2 object to call the format method from the DecimalFormat class.
         System.out.println("The number rounded to 3 decimals is " + roundTo3.format(number));
+
+
+        DecimalFormat currency = DecimalFormat("$#, ###.00");
+        System.out.println("the money i have is "+ currency.format(number));
+        System.out.println("the money i have is "+ currency.format(11));
     }
 }
